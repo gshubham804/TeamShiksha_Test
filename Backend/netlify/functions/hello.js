054f7085945e16ee8724065bfadf8e1bf6fdd2e7
gshubham804 <gshubham804@gmail.com>
2025-11-29 14:53:03 +0530

    chore: Remove Netlify configuration and related serverless function files

--- Backend/netlify/functions/hello.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello! Backend is working on Netlify",
-    }),
-  };
-};
-
